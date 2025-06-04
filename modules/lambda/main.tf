cd7c338d77191b8d074682deb814c34d46611909
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-04 17:47:07 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,8 +1,8 @@
-/*data "archive_file" "lambda_zip" {
+data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/files"
   output_path = "${path.module}/lambda.zip"
-} */
+} 
 
 resource "aws_lambda_function" "image_resizer" {
   function_name    = var.function_name

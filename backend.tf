6890ced6775a10d0baceaed240b71ba28dab9b12
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-04 09:59:45 +0000

    Use case 5 init commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-oidc-bucket-15328069840"
+    key            = "two-tier-app/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
